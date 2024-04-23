4cfea7bf1625150f9b7bd467a13a444dfe5f7cbe
Philip I <24440690+phibiam@users.noreply.github.com>
2024-04-23 13:47:05 -0600

    Update watcher.sh

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,22 @@
-#TODO
+```bash
+#!/bin/bash
+
+# Define variables
+namespace="your-namespace"
+deployment="your-deployment"
+max_restarts=3
+
+while true; do
+restarts=$(kubectl get pods -n $namespace | grep $deployment | awk '{print $4}')
+
+echo "Current restart count: $restarts"
+
+if [ $restarts -gt $max_restarts ]; then
+echo "Restart count exceeded the limit. Scaling down deployment..."
+kubectl scale deployment $deployment --replicas=0 -n $namespace
+break
+fi
+
+sleep60
+done
+```
